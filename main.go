67c7667d673004d09ca2deba9a3b671b7ab3a407
Richard Jones <itszootime@gmail.com>
2016-04-07 22:06:42 +0800

    go fmt

--- main.go
+++ main.go
@@ -12,78 +12,77 @@ package main
  */
 
 import (
-  "flag"
-  "fmt"
-  "github.com/samuel/go-zookeeper/zk"
-  "strings"
-  "time"
-  "encoding/json"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/samuel/go-zookeeper/zk"
+	"strings"
+	"time"
 )
 
-
 type Setup struct {
-  basePath string
-  zk string
-  zkRoot string
+	basePath string
+	zk       string
+	zkRoot   string
 }
 
 var setup = Setup{}
 
 func init() {
-  flag.StringVar(&setup.basePath, "base-path", "", "base path")
-  flag.StringVar(&setup.zk, "zk", "127.0.0.1:2181", "zk")
-  flag.StringVar(&setup.zkRoot, "zk-root", "/zconfig", "zk root")
+	flag.StringVar(&setup.basePath, "base-path", "", "base path")
+	flag.StringVar(&setup.zk, "zk", "127.0.0.1:2181", "zk")
+	flag.StringVar(&setup.zkRoot, "zk-root", "/zconfig", "zk root")
 }
 
 func iferr(err error) {
-  if err != nil {
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
-  flag.Parse()
+	flag.Parse()
 
-  conn := zkConnect()
-  defer conn.Close()
-  zkInit(conn)
+	conn := zkConnect()
+	defer conn.Close()
+	zkInit(conn)
 
-  printValues(conn, setup.zkRoot)
+	printValues(conn, setup.zkRoot)
 
-  // get and watch root
-  changes, errors := watch(conn, setup.zkRoot)
-  for {
-    select {
-    case change := <-changes:
-      fmt.Printf("main:change path=%v\n", change)
-      printValues(conn, setup.zkRoot)
-    case <-errors:
-      // we'll end up with node does not exist here
-      // which will kill the go routine (it's fine)
-      // fmt.Printf("main:error error=%v\n", err)
-    }
-  }
+	// get and watch root
+	changes, errors := watch(conn, setup.zkRoot)
+	for {
+		select {
+		case change := <-changes:
+			fmt.Printf("main:change path=%v\n", change)
+			printValues(conn, setup.zkRoot)
+		case <-errors:
+			// we'll end up with node does not exist here
+			// which will kill the go routine (it's fine)
+			// fmt.Printf("main:error error=%v\n", err)
+		}
+	}
 }
 
 func zkConnect() *zk.Conn {
-  // TODO: allow configurable timeout?
-  conn, _, err := zk.Connect(strings.Split(setup.zk, ","), time.Second)
-  iferr(err) // severe
-  return conn
+	// TODO: allow configurable timeout?
+	conn, _, err := zk.Connect(strings.Split(setup.zk, ","), time.Second)
+	iferr(err) // severe
+	return conn
 }
 
 func zkInit(conn *zk.Conn) {
-  // TODO: ensure these flags are correct
-  flags := int32(0)
-  acl := zk.WorldACL(zk.PermAll)
+	// TODO: ensure these flags are correct
+	flags := int32(0)
+	acl := zk.WorldACL(zk.PermAll)
 
-  exists, _, err := conn.Exists(setup.zkRoot)
-  iferr(err) // severe
-  if !exists {
-    // TODO: ignore node already exists here
-    _, err := conn.Create(setup.zkRoot, nil, flags, acl)
-    iferr(err)
-  }
+	exists, _, err := conn.Exists(setup.zkRoot)
+	iferr(err) // severe
+	if !exists {
+		// TODO: ignore node already exists here
+		_, err := conn.Create(setup.zkRoot, nil, flags, acl)
+		iferr(err)
+	}
 }
 
 // TODO: move!
@@ -100,147 +99,147 @@ func zkInit(conn *zk.Conn) {
 
 // TODO: every value is a string, is this a problem? (it's hard to fix)
 func fetchValues(conn *zk.Conn, path string) (map[string]interface{}, error) {
-  v := make(map[string]interface{})
+	v := make(map[string]interface{})
 
-  // get children
-  children, _, err := conn.Children(path)
-  if err != nil {
-    // TODO: what errors? maybe the error just means empty value?
-    return nil, err
-  }
+	// get children
+	children, _, err := conn.Children(path)
+	if err != nil {
+		// TODO: what errors? maybe the error just means empty value?
+		return nil, err
+	}
 
-  if len(children) > 0 {
-    for i := range children {
-      childpath := path + "/" + children[i]
-      childchildren, _, err := conn.Children(childpath)
-      if err != nil {
-        // TODO: what errors? maybe the error just means empty value?
-        return nil, err
-      }
+	if len(children) > 0 {
+		for i := range children {
+			childpath := path + "/" + children[i]
+			childchildren, _, err := conn.Children(childpath)
+			if err != nil {
+				// TODO: what errors? maybe the error just means empty value?
+				return nil, err
+			}
 
-      if len(childchildren) == 0 {
-        // value
-        bytes, _, err := conn.Get(childpath)
-        if err != nil {
-          // TODO: what errors? maybe the error just means empty value?
-          return nil, err
-        }
-        v[children[i]] = string(bytes)
-      } else {
-        // could be an array of values, or could be recursive
-        childvalues, err := fetchValues(conn, childpath)
-        if err != nil {
-          // TODO: errors
-          return nil, err
-        }
+			if len(childchildren) == 0 {
+				// value
+				bytes, _, err := conn.Get(childpath)
+				if err != nil {
+					// TODO: what errors? maybe the error just means empty value?
+					return nil, err
+				}
+				v[children[i]] = string(bytes)
+			} else {
+				// could be an array of values, or could be recursive
+				childvalues, err := fetchValues(conn, childpath)
+				if err != nil {
+					// TODO: errors
+					return nil, err
+				}
 
-        // the challenge here is how to decide if this is an array
-        // if all values are empty strings, it's an array
-        // TODO: document this logic, it can be strange under certain conditions
-        valuesarr := make([]string, 0, len(childvalues))
-        isarr := true
-        for k, v := range childvalues {
-          // TODO: seems hacky
-          if len(fmt.Sprintf("%v", v)) > 0 {
-            isarr = false
-            break
-          }
-          valuesarr = append(valuesarr, k)
-        }
+				// the challenge here is how to decide if this is an array
+				// if all values are empty strings, it's an array
+				// TODO: document this logic, it can be strange under certain conditions
+				valuesarr := make([]string, 0, len(childvalues))
+				isarr := true
+				for k, v := range childvalues {
+					// TODO: seems hacky
+					if len(fmt.Sprintf("%v", v)) > 0 {
+						isarr = false
+						break
+					}
+					valuesarr = append(valuesarr, k)
+				}
 
-        if isarr {
-          v[children[i]] = valuesarr
-        } else {
-          v[children[i]] = childvalues
-        }
-      }
-    }
-  }
+				if isarr {
+					v[children[i]] = valuesarr
+				} else {
+					v[children[i]] = childvalues
+				}
+			}
+		}
+	}
 
-  return v, nil
+	return v, nil
 }
 
 func watchValue(conn *zk.Conn, path string, changes chan string, errors chan error) {
-  for {
-    _, _, events, err := conn.GetW(path)
-    if err != nil {
-      errors <- err
-      return
-    }
+	for {
+		_, _, events, err := conn.GetW(path)
+		if err != nil {
+			errors <- err
+			return
+		}
 
-    evt := <-events
-    // TODO: zk node does not exist is normal
-    if evt.Err != nil {
-      errors <- evt.Err
-      return
-    }
+		evt := <-events
+		// TODO: zk node does not exist is normal
+		if evt.Err != nil {
+			errors <- evt.Err
+			return
+		}
 
-    changes <- path
-  }
+		changes <- path
+	}
 }
 
 func watchTree(conn *zk.Conn, path string, changes chan string, errors chan error) {
-  // TODO: don't need value watches on level 0 (i.e. /zconfig/servers)
-  // TODO: this can also build the config
-  treeWatches := make(map[string]bool)
-  valueWatches := make(map[string]bool)
+	// TODO: don't need value watches on level 0 (i.e. /zconfig/servers)
+	// TODO: this can also build the config
+	treeWatches := make(map[string]bool)
+	valueWatches := make(map[string]bool)
 
-  for {
-    children, _, events, err := conn.ChildrenW(path)
-    if err != nil {
-      errors <- err
-      return
-    }
+	for {
+		children, _, events, err := conn.ChildrenW(path)
+		if err != nil {
+			errors <- err
+			return
+		}
 
-    for i := range children {
-      child := children[i]
-      childpath := path + "/" + child
+		for i := range children {
+			child := children[i]
+			childpath := path + "/" + child
 
-      // watch tree
-      if !treeWatches[child] {
-        treeWatches[child] = true
-        go func() {
-          defer delete(treeWatches, child)
-          watchTree(conn, childpath, changes, errors)
-        }()
-      }
+			// watch tree
+			if !treeWatches[child] {
+				treeWatches[child] = true
+				go func() {
+					defer delete(treeWatches, child)
+					watchTree(conn, childpath, changes, errors)
+				}()
+			}
 
-      // watch values
-      if !valueWatches[child] {
-        valueWatches[child] = true
-        go func() {
-          defer delete(valueWatches, child)
-          watchValue(conn, childpath, changes, errors)
-        }()
-      }
-    }
+			// watch values
+			if !valueWatches[child] {
+				valueWatches[child] = true
+				go func() {
+					defer delete(valueWatches, child)
+					watchValue(conn, childpath, changes, errors)
+				}()
+			}
+		}
 
-    evt := <-events
-    // TODO: zk node does not exist is expected
-    if evt.Err != nil {
-      errors <- evt.Err
-      return
-    }
+		evt := <-events
+		// TODO: zk node does not exist is expected
+		if evt.Err != nil {
+			errors <- evt.Err
+			return
+		}
 
-    changes <- path
-  }
+		changes <- path
+	}
 }
 
 func printValues(conn *zk.Conn, path string) {
-  values, err := fetchValues(conn, path)
-  iferr(err)
-  bytes, err := json.Marshal(values)
-  iferr(err)
-  fmt.Printf("%v\n", string(bytes))
+	values, err := fetchValues(conn, path)
+	iferr(err)
+	bytes, err := json.Marshal(values)
+	iferr(err)
+	fmt.Printf("%v\n", string(bytes))
 }
 
 func watch(conn *zk.Conn, path string) (chan string, chan error) {
-  changes := make(chan string)
-  errors := make(chan error)
+	changes := make(chan string)
+	errors := make(chan error)
 
-  go func() {
-    watchTree(conn, path, changes, errors)
-  }()
+	go func() {
+		watchTree(conn, path, changes, errors)
+	}()
 
-  return changes, errors
+	return changes, errors
 }
