4e2e0eb956d99ff68f0927c4a8d8911fef7fe19b
Richard Jones <itszootime@gmail.com>
2016-04-10 16:00:44 +0800

    clearer tree/value watch tracking

--- watcher.go
+++ watcher.go
@@ -10,10 +10,20 @@ type Watcher struct {
 
 	changes chan string
 	errors  chan error
+
+	tree  map[string]bool
+	value map[string]bool
 }
 
 func NewWatcher(conn *zk.Conn, path string) *Watcher {
-	return &Watcher{conn, path, make(chan string), make(chan error)}
+	return &Watcher{
+		conn:    conn,
+		path:    path,
+		changes: make(chan string),
+		errors:  make(chan error),
+		tree:    make(map[string]bool),
+		value:   make(map[string]bool),
+	}
 }
 
 func (w *Watcher) Start() (chan string, chan error) {
@@ -26,11 +36,20 @@ func (w *Watcher) Start() (chan string, chan error) {
 
 func (w *Watcher) Stop() {
 	// TODO: signal to terminate zk watches
+	// TODO: clear tree/value maps
 	close(w.changes)
 	close(w.errors)
 }
 
 func (w *Watcher) watchValue(path string) {
+	// TODO: mutex here
+	if _, ok := w.value[path]; ok {
+		return
+	}
+
+	w.value[path] = true
+	defer delete(w.value, path)
+
 	for {
 		_, _, events, err := w.conn.GetW(path)
 		if err != nil {
@@ -50,11 +69,15 @@ func (w *Watcher) watchValue(path string) {
 }
 
 func (w *Watcher) watchTree(path string) {
-	// TODO: don't need value watches on level 0 (i.e. /zconfig/servers)
-	// TODO: this can also build the config
-	treeWatches := make(map[string]bool)
-	valueWatches := make(map[string]bool)
+	// TODO: mutex here
+	if _, ok := w.tree[path]; ok {
+		return
+	}
 
+	w.tree[path] = true
+	defer delete(w.tree, path)
+
+	// TODO: don't need value watches on level 0 (i.e. /zconfig/servers)
 	for {
 		children, _, events, err := w.conn.ChildrenW(path)
 		if err != nil {
@@ -65,24 +88,8 @@ func (w *Watcher) watchTree(path string) {
 		for i := range children {
 			child := children[i]
 			childpath := path + "/" + child
-
-			// watch tree
-			if !treeWatches[child] {
-				treeWatches[child] = true
-				go func() {
-					defer delete(treeWatches, child)
-					w.watchTree(childpath)
-				}()
-			}
-
-			// watch values
-			if !valueWatches[child] {
-				valueWatches[child] = true
-				go func() {
-					defer delete(valueWatches, child)
-					w.watchValue(childpath)
-				}()
-			}
+			go w.watchTree(childpath)
+			go w.watchValue(childpath)
 		}
 
 		evt := <-events
